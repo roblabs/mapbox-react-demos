e2ef4eafdc79ff3d7ea58f78d0b46e41fd21c124
Rob | Labs <roblabs@users.noreply.github.com>
2018-09-10 07:18:29 -0700

    Mapbox Assembly

--- src/index.js
+++ src/index.js
@@ -4,5 +4,23 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+ReactDOM.render(
+  
+<nav>
+  <a class='flex-parent-inline btn color-blue color-white-on-active bg-transparent bg-darken10-on-hover bg-blue-on-active txt-s ml3 is-active' href='#'>
+    <svg class='icon mr3'><use xlinkHref='#icon-home'/></svg> Home
+  </a>
+  <a class='flex-parent-inline btn color-blue color-white-on-active bg-transparent bg-darken10-on-hover bg-blue-on-active txt-s ml3' href='#'>
+    <svg class='icon mr3'><use xlinkHref='#icon-user'/></svg> Account
+  </a>
+  <a class='flex-parent-inline btn color-blue color-white-on-active bg-transparent bg-darken10-on-hover bg-blue-on-active txt-s ml3' href='#'>
+    <svg class='icon mr3'><use xlinkHref='#icon-logout'/></svg> Logout
+  </a>
+</nav>
+,
+  document.getElementById('root')
+);
+
+ReactDOM.render(<App />, document.body.appendChild(document.createElement('div')));
 registerServiceWorker();
